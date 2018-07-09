a942fbbb4c30c8533a2f4b1760a48ba86fc55ac3
Ron Mac <texx.ronn@gmail.com>
2018-07-09 20:00:31 +1000

    Tryin gunicorn

---
+++ genericCrossFilterRecipe.py
@@ -0,0 +1,12 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import numpy as np
+import pandas as pd
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
+
+np.random.seed(0)
+
+df = pd.
